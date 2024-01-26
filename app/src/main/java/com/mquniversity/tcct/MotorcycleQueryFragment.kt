a751eb57dd5c2aaa05fb31d2b72c9b0624a468fc
JaydenKing32 <jaydenking32@gmail.com>
2024-01-26 21:57:23 +1100

    Added default motorcycle settings

--- app/src/main/java/com/mquniversity/tcct/MotorcycleQueryFragment.kt
+++ app/src/main/java/com/mquniversity/tcct/MotorcycleQueryFragment.kt
@@ -5,6 +5,7 @@ import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
@@ -18,6 +19,17 @@ class MotorcycleQueryFragment : PrivateVehicleQueryFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val specifiedBike = pref.getBoolean(getString(R.string.use_specified_motorcycle_key), false)
+
+        if (specifiedBike) {
+            setupResult(
+                pref.getString(getString(R.string.specified_motorcycle_size_key), getString(R.string.specified_motorcycle_size_default))!!
+            )
+            return
+        }
+
         headerText.text = getString(R.string.motorcycle)
         sizeDescBtn.setOnClickListener {
             MaterialAlertDialogBuilder(requireContext())
@@ -41,24 +53,7 @@ class MotorcycleQueryFragment : PrivateVehicleQueryFragment() {
         }
 
         calBtn.setOnClickListener {
-            mainActivity.bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            calBtn.isEnabled = false
-            var motorcycleResultFrag =
-                parentFragmentManager.findFragmentByTag(getString(R.string.tag_motorcycle_result)) as MotorcycleResultFragment?
-            if (motorcycleResultFrag == null) {
-                mainActivity.enableButtons(false)
-                motorcycleResultFrag = MotorcycleResultFragment(sizeOptions[currSizeIdx])
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, motorcycleResultFrag, getString(R.string.tag_motorcycle_result))
-                    .addToBackStack(getString(R.string.tag_motorcycle_result))
-                    .commit()
-            } else {
-                motorcycleResultFrag.updateFactor(sizeOptions[currSizeIdx])
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, motorcycleResultFrag, getString(R.string.tag_motorcycle_result))
-                    .commit()
-            }
-            calBtn.isEnabled = true
+            setupResult(sizeOptions[currSizeIdx])
         }
     }
 
@@ -68,5 +63,25 @@ class MotorcycleQueryFragment : PrivateVehicleQueryFragment() {
         savedInstanceState: Bundle?
     ): View {
         return mainLayout
+    }
+
+    private fun setupResult(size: String) {
+        mainActivity.bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        calBtn.isEnabled = false
+        var bikeResultFrag = parentFragmentManager.findFragmentByTag(getString(R.string.tag_motorcycle_result)) as MotorcycleResultFragment?
+        if (bikeResultFrag == null) {
+            mainActivity.enableButtons(false)
+            bikeResultFrag = MotorcycleResultFragment(size)
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, bikeResultFrag, getString(R.string.tag_motorcycle_result))
+                .addToBackStack(getString(R.string.tag_motorcycle_result))
+                .commit()
+        } else {
+            bikeResultFrag.updateFactor(size)
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, bikeResultFrag, getString(R.string.tag_motorcycle_result))
+                .commit()
+        }
+        calBtn.isEnabled = true
     }
 }
